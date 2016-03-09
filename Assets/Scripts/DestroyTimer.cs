283a8331d48b24224e9c3f6ceeb0c43a38e4d30c
HakonHesselberg <hakon.hesselberg@gmail.com>
2016-03-09 16:39:54 +0100

    Fixed ShootRight script to not spawn duplicates. Added DestroyTimer script to Bullet prefab so it despawns after 5 sec

---
+++ Assets/Scripts/DestroyTimer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyTimer : MonoBehaviour {
+
+    public float destroyTimer;
+
+    void Start()
+    {
+        Destroy(transform.gameObject, destroyTimer);
+    }
+}
--- Assets/Scripts/ShootRight.cs
+++ Assets/Scripts/ShootRight.cs
@@ -5,13 +5,30 @@ public class ShootRight : MonoBehaviour {
 
     public GameObject Bullet;
     public Transform shotSpawn;
-	
+    public float maxDelay = 0.2f;
+
+    private float delayTimer = 0f;
+
 	// Update is called once per frame
 	void Update () {
         if (Input.GetButton("Fire1"))
         {
-            Instantiate(Bullet, shotSpawn.position, shotSpawn.rotation);
-            GameObject clone = Instantiate(Bullet, transform.position, transform.rotation) as GameObject;
+            if (delayTimer > maxDelay)
+            {
+                Instantiate(Bullet, shotSpawn.position, shotSpawn.rotation);
+                delayTimer = 0;
+            }
+
+            // Til Vegard - Denne var det som gjorde at det kom for mange kuler, siden når du lager et gameObject med
+            // Instantiate(...) så lager du den faktisk i spillet. Når du tar GameObject clone = Instantiate(...) så
+            // får du laget objectet, i tillegg til at du setter variablen "clone" til å bli det gameObjectet. Dette
+            // er nyttig hvis du for eksempel skal endre farge eller position på kulen du spawnet senere i scriptet,
+            // eller få den til å eksplodere etterhvert. Eksempel clone.deathTimer(10) <-- I just made this up, not
+            // real code
+
+            //GameObject clone = Instantiate(Bullet, transform.position, transform.rotation) as GameObject;
         }
+        delayTimer += Time.deltaTime;
+
 	}
 }
