3cc71761fc759c1d16de4b8d9022cf09f822d886
Vegard Hesselberg <vegard.hesselberg@gmail.com>
2016-03-14 17:59:04 +0100

    Managed to turn player to mouse. Good job team.

---
+++ Assets/Scripts/PlayerRotation.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerRotation : MonoBehaviour
+{
+    private float hitDist;
+    private Vector3 targetPoint;
+    CharacterController cc;
+
+    void Start()
+    {
+        hitDist = 0;
+        cc = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        var playerPlane = new Plane(Vector3.up, transform.position);
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (playerPlane.Raycast(ray, out hitDist))
+        {
+            targetPoint = ray.GetPoint(hitDist);
+        }
+
+        cc.transform.LookAt(targetPoint);
+    }
+}
